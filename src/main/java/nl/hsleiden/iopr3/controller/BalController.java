afc5d0a7a08f31583b51d811d21c7c3eff13bbdb
Jordi Gräffner <jordi_g_92@hotmail.com>
2014-10-26 15:22:39 +0100

    d

--- src/main/java/nl/hsleiden/iopr3/controller/BalController.java
+++ src/main/java/nl/hsleiden/iopr3/controller/BalController.java
@@ -14,6 +14,7 @@ import java.awt.event.MouseWheelListener;
 public class BalController implements Runnable, MouseWheelListener {
     private final Bal bal;
     private final BalView balview;
+    private final BalView balview;
     private final ValBewegingPaneel valBewegingPaneel;
     private final ControlePaneelNoord noordpaneel;
 
